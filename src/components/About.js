5073d823961bff58aefc542ca793c7cfe6d6ef4d
ken <kenmugy@gmail.com>
2020-01-03 21:02:07 +0300

    simple react router practice

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className='container'>
+      <h1 className='red-text'>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A ea maiores
+        suscipit reiciendis, quasi earum non iste mollitia eaque sed praesentium
+        magni deserunt incidunt. Corrupti nemo sunt perspiciatis sequi officii
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Navs.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navs() {
+  return (
+    <nav className='indigo'>
+      <div className='container'>
+        <a href className='brand-logo left'>
+          Demo
+        </a>
+        <ul className='right'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contacts'>contacts</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
